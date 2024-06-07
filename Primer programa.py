fa1634698fbf8b9ec32afc96d5629b4e2e1a9eb7
gustavo-diaz-es <51009253+gustavo-diaz-es@users.noreply.github.com>
2024-06-06 20:13:38 -0600

    Add files via upload
    Esta es una prueba de un archivo con informacion de NumPy

---
+++ Primer programa.py
@@ -0,0 +1,17 @@
+﻿* Numpy es la libreria mejor utilizada para manejo de arreglos y matrices.
+
+
+import numpy as np
+# create a 1D numpy array of 2-byte floats
+float_array = ___.___(___, ___=___)
+# create a 2D numpy array with 2 arrays of 1-byte integers
+array_2d = ___.___(___, ___=___)
+print(float_array)
+print('-' * 12)
+print(array_2d)
+
+
+solucion:
+¿Has utilizado la función np.array() con los argumentos correctos para crear una matriz 1D? Esperado: float_array = np.array([1.32, 4.6], dtype=np.float16). 
+¿Has utilizado la función np.array() con los argumentos correctos para crear un array numpy 2D?
+Esperado: array_2d = np.array([[24, 41], [32, 25]], dtype=np.int8).
